31ae34c47db4358a3abe8f96e15548f8dc1ef213
Rishaviq <95432233+Rishaviq@users.noreply.github.com>
2022-03-28 14:53:34 +0300

    Add files via upload

---
+++ programming zad/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programming_zad
+{
+   public class product {
+
+        public string name { get; set; } = "";
+
+        public decimal price
+        {
+            get;
+            set;
+        } = 0;
+
+
+    }
+}
